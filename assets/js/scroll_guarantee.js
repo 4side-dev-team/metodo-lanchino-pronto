f225ee875204f27bb42d24f9ae0e322b662f4d86
4side-dev-team <4side.contato@gmail.com>
2026-02-11 17:21:18 -0300

    organiza estrutura para vercel

---
+++ assets/js/scroll_guarantee.js
@@ -0,0 +1,6 @@
+setTimeout(() => {
+  const section = document.querySelector('.guarantee');
+  if (section) {
+    section.scrollIntoView({ behavior: 'auto', block: 'center' });
+  }
+}, 500);
--- scroll_guarantee (copy).js
+++
@@ -1,6 +0,0 @@
-setTimeout(() => {
-  const section = document.querySelector('.guarantee');
-  if (section) {
-    section.scrollIntoView({ behavior: 'auto', block: 'center' });
-  }
-}, 500);
